28b9a5f980d80fda0421d90a0203def318649b15
Richard Vieira <richard.vieiraes@gmail.com>
2024-11-04 19:45:46 -0400

    aula 08

--- aula08_arquivo/aula08.py
+++ aula08_arquivo/aula08.py
@@ -1,7 +1,32 @@
-arquivo =open("aula08_arquivo/monstros.txt","r")
+#>>>Parte 1
+#arquivo = open("aula08_arquivo/monstros.txt","r")
+#print(arquivo.readable())
+# print(arquivo.read())
+# print(arquivo.readline())
+# print(arquivo.readline())
+#arquivo.readlines()
+# lista = arquivo.readlines()
+# # print(lista)
+# print(lista[1])
+# print(lista[2])
+#arquivo.close()
 
-lista=arquivo.readlines()
 
-print(lista[3])
+#>>>Parte 2
+# arquivo = open("aula08_arquivo/monstros.txt","a")
+# arquivo.write("Texto\n")
+# arquivo.close()
 
-arquivo.close()
+#>>>Parte 3
+#arquivo = open("aula08_arquivo/monstros.txt","w")
+# arquivo.write("Texto\n")
+# arquivo.close()
+
+#>>>Prte 4
+# arquivo = open("aula08_arquivo/monstros2.txt","r+")
+# texto=arquivo.read()
+# print(texto)
+# arquivo.write("ola \n")
+# texto=arquivo.read()
+# print(texto)
+#arquivo.close()
--- aula08_arquivo/teste.py
+++ aula08_arquivo/teste.py
@@ -1,29 +1,4 @@
-#>>PARTE 1
-#open("caminho","tipo")
-# arquivo=open("aula08_arquivo/teste.txt","w")
-# arquivo.write("Olá mundo")
-# arquivo.close()
-
-# >>> Parte 2
-# arquivo=open("aula08_arquivo/teste.txt","r")
-# print(arquivo.readable())
-# #print(arquivo.read())
-# #print(arquivo.readline())
-# lista=arquivo.readlines()
-# print(lista[0])
-# arquivo.close()
-
-#>>Parte 3
-# arquivo = open("aula08_arquivo/teste.txt","a")
-# arquivo.write("\nTexto")
-# arquivo.close()
-
-#>>Parte 4 
 import ast
-
-# Abre o arquivo e lê o conteúdo
-# with open("aula08_arquivo/teste.txt", "r") as arquivo:
-#     conteudo = arquivo.read()
 
 arquivo=open("aula08_arquivo/teste.txt", "r")
 conteudo=arquivo.read()
@@ -38,4 +13,5 @@ mana = jogador["mana"]
 print(f"Nome: {nome}")
 print(f"HP: {hp}")
 print(f"Mana: {mana}")
+
 arquivo.close()
